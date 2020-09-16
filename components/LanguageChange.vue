251f869fb43360952e31703e471076446a791f30
Pedro Padron <padronpedro@gmail.com>
2020-09-16 16:34:22 -0400

    New components for menu items and language items

---
+++ components/LanguageChange.vue
@@ -0,0 +1,30 @@
+<template>
+  <NuxtLink :to="switchLocalePath(changeLang)">
+    <v-list-item>
+      <v-list-item-icon>
+        <v-img :src="require('~/assets/flag-' + changeLang + '.png')" width="20px" />
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title v-text="langTitle"></v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  name: 'LanguageChange',
+  props: {
+    langTitle: {
+      type: String,
+      required: true,
+    },
+    changeLang: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ components/MenuItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <NuxtLink :to="localePath(menuPath)">
+    <v-list-item>
+      <v-list-item-icon v-if="menuIcon">
+        <v-icon v-text="'mdi-' + menuIcon"></v-icon>
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title v-text="menuTitle"></v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  name: 'MenuItem',
+  props: {
+    menuTitle: {
+      type: String,
+      required: true,
+    },
+    menuPath: {
+      type: String,
+      required: true,
+    },
+    menuIcon: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style></style>
