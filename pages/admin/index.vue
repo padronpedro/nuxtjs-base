d067d03a0885168ebe8785f37b5a9a2c9bba3011
Pedro Padron <padronpedro@gmail.com>
2020-09-18 17:10:37 -0400

    Admin login/password page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
-    <v-text-field v-model="email" label="email"></v-text-field>
-    <v-text-field v-model="password" label="password"></v-text-field>
-    <button @click="accede">Login</button>
-    <button @click="logout">Logout</button>
-    <button @click="$router.push('/')">Back</button>
+    <v-card class="mx-auto" max-width="400">
+      <v-card-title>{{ $t('Welcome') }}</v-card-title>
+
+      <v-card-text class="text--primary">
+        <v-text-field v-model="email" type="email" :label="$t('Email')" autocomplete="new-password"></v-text-field>
+        <v-text-field v-model="password" type="password" :label="$t('Password')"></v-text-field>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn text @click="login">
+          {{ $t('Login') }}
+        </v-btn>
+
+        <v-btn color="red" text @click="$router.push('/')">
+          {{ $t('Exit') }}
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
@@ -18,8 +32,13 @@ export default {
       password: '',
     }
   },
+  mounted() {
+    if (this.$auth.loggedIn) {
+      this.$router.push('/admin/dashboard')
+    }
+  },
   methods: {
-    async accede() {
+    async login() {
       try {
         await this.$auth.loginWith('local', {
           data: {
@@ -27,13 +46,11 @@ export default {
             password: this.password,
           },
         })
-        this.$router.push('/')
+        this.$store.setUser(this.$auth.user)
+        this.$router.push('/admin/dashboard')
       } catch (e) {
-        this.error = e.response.data.message
+        this.error = e
       }
-    },
-    async logout() {
-      await this.$auth.logout()
     },
   },
 }
